8e2efaf69e8a1771ed3cdf6db82290dfb8f9b9de
Duc Anh <khamvl08@gmail.com>
2025-01-07 10:02:17 +0700

    fix class

--- src/Class/Attendance.java
+++ src/Class/Attendance.java
@@ -5,21 +5,18 @@ import javafx.beans.property.StringProperty;
 
 public class Attendance {
     private int attendanceID;
-    private Class aClass;
-    private Student student;
+    private int classID;
+    private int studentID;
     private String date;
     private String status;
     private StringProperty dynamic = new SimpleStringProperty("Present");;
 
-    public Attendance(Student student, Class aClass, String status) {
-        this.student = student;
-        this.status = status;
-    }
+    
     // Constructor
-    public Attendance(int attendanceID, Class aClass, Student student, String date, String status) {
+    public Attendance(int attendanceID, int classID, int studentID, String date, String status) {
         this.attendanceID = attendanceID;
-        this.aClass = aClass;
-        this.student = student;
+        this.classID = classID;
+        this.studentID = studentID;
         this.date = date;
         this.status = status;
     }
@@ -33,24 +30,26 @@ public class Attendance {
         this.attendanceID = attendanceID;
     }
 
-    public Class getaClass() {
-        return aClass;
-    }
-
-    public void setClass(Class aClass) {
-        this.aClass = aClass;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-    }
+    
 
     public String getDate() {
         return date;
+    }
+
+    public int getClassID() {
+        return classID;
+    }
+
+    public void setClassID(int classID) {
+        this.classID = classID;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
     }
 
     public void setDate(String date) {
@@ -63,9 +62,6 @@ public class Attendance {
 
     public void setStatus(String status) {
         this.status = status;
-    }
-    public void setaClass(Class aClass) {
-        this.aClass = aClass;
     }
     public String getDynamicValue() {
         return dynamic.get();
